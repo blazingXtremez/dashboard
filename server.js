2f13047c490f249debc135100aef2672d27dee14
Riteish <riteishy2j@gmail.com>
2016-12-03 11:32:51 -0500

    made some random changes

--- server.js
+++ server.js
@@ -4,13 +4,23 @@ const express = require('express');
 // declaring the app as part of express
 var app = express();
 
+
+// setting app level middleware that will log in to console
+app.use((req, res, next) => {
+	console.log("request made to ", req.url);
+	next();
+});
+
 // setting a starting route
 app.get('/', (req, res) => {
-	res.send("Hello this is a test");
+	res.sendFile(__dirname
+	 + '/public/index.html');
 });
 
 // create some more routes
-
+app.get('/test' , (req, res) => {
+	res.send("This is a new page");
+});
 
 // some milddleware to handle API calls for database
 
@@ -22,4 +32,9 @@ app.listen(3000, () => {
 });
 
 
-
+// stuff to do 
+// route for various page?
+// schema and model for mongoose dbt
+// test mongoose db
+// apis to get and post from mongoose db
+// front end
